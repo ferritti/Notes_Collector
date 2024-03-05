7a587de814e7c105c7d458be00d77589995a9cad
Andrea Ferritti <126817248+ferritti@users.noreply.github.com>
2024-03-05 17:14:28 +0100

    Main update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include "Note.h"
+#include "NotesCollection.h"
+#include "ControlPanel.h"
+#include <list>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::shared_ptr<Note> Note1 = std::make_shared<Note>("Note 1", "This is the first note.");
+    std::shared_ptr<Note> Note2 = std::make_shared<Note>("Note 2", "This is the second note.");
+
+    auto collection1 = new NotesCollection("First Collection");
+    auto collection2 = new NotesCollection("Second Collection");
+
+    collection1->addNote(Note1);
+    collection1->addNote(Note2);
+
+    collection1->removeNote("Note 1");
+
+    std::cout << "First collection has: " << collection1->getNoteNum() << " notes" << "\n" << std::endl;
+    std::cout << "Second collection has: " << collection2->getNoteNum() << " notes" << "\n" << std::endl;
+
+    auto collection3 = new NotesCollection("Third Collection");
+
+    auto controlPanel = new ControlPanel({collection1, collection2});
+    controlPanel->addCollection(collection3);
+    std::cout << "Control panel has: " << controlPanel->getCollectionNum() << " collections" << std::endl;
+
     return 0;
 }
 
